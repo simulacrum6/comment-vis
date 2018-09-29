a5fd32bce704c6c7bcf77afc5a36fae070f6a3dc
Marius Hamacher <marius.hamacher@outlook.de>
2018-09-29 15:17:27 +0200

    add PDFParser to Upload

--- src/main/java/awkwardrobots/util/FeedbackUploader.java
+++ src/main/java/awkwardrobots/util/FeedbackUploader.java
@@ -12,18 +12,33 @@ import java.util.List;
 
 public class FeedbackUploader implements Upload.Receiver, Upload.SucceededListener {
 
+    private String mimeType;
     private ByteArrayOutputStream outputStream;
 
     @Override
     public OutputStream receiveUpload(String filename, String mimeType) {
+        this.mimeType = mimeType;
         return outputStream = new ByteArrayOutputStream();
     }
 
     @Override
     public void uploadSucceeded(Upload.SucceededEvent event) {
-        CommentParser commentParser = new TXTCommentParser();
+        CommentParser commentParser = findMatchingParser();
         List<Comment> comments = commentParser.parse(new ByteArrayInputStream(outputStream.toByteArray()));
         ((Main) UI.getCurrent()).getDashboardView().setComments(comments);
         UI.getCurrent().getNavigator().navigateTo("Dashboard");
+    }
+
+    private CommentParser findMatchingParser() {
+        CommentParser commentParser = null;
+        switch (mimeType) {
+            case "application/pdf":
+                commentParser = new EvaluationPDFParser();
+                break;
+            case "text/plain":
+                commentParser = new TXTCommentParser();
+                break;
+        }
+        return commentParser;
     }
 }
