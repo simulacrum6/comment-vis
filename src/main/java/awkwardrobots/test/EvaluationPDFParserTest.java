f00e7c3850e18c0c5052c942e91af88176ff3e23
Marius Hamacher <marius.hamacher@outlook.de>
2018-09-29 12:40:19 +0200

    add dirty implementation

--- src/main/java/awkwardrobots/test/EvaluationPDFParserTest.java
+++ src/main/java/awkwardrobots/test/EvaluationPDFParserTest.java
@@ -3,23 +3,40 @@ package awkwardrobots.test;
 import awkwardrobots.data.Comment;
 import awkwardrobots.util.CommentParser;
 import awkwardrobots.util.EvaluationPDFParser;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class EvaluationPDFParserTest {
+    private CommentParser parser;
+    private FileInputStream in;
+    private List<Comment> comments;
+
+    @Before
+    public void init() throws FileNotFoundException {
+        parser = new EvaluationPDFParser();
+        in = new FileInputStream("src/main/resources/evaluations/sprachtechnologie.pdf");
+    }
 
     @Test
-    public void testPDFParsing() throws FileNotFoundException {
-        CommentParser parser = new EvaluationPDFParser();
-        FileInputStream in = new FileInputStream("src/main/resources/evaluations/sprachtechnologie.pdf");
+    public void testPDFParsing() {
         List<Comment> comments = parser.parse(in);
         assertNotNull(comments);
+    }
 
-        System.out.println(comments);
+    @Test
+    public void testCommentExtraction() {
+        List<Comment> comments = parser.parse(in);
+        Comment first = comments.get(0);
+        assertEquals("Keine Angabe", first.getText());
+
+        Comment last = comments.get(comments.size() - 1);
+        assertEquals("weniger programmieren", last.getText());
     }
 }
--- src/main/java/awkwardrobots/util/EvaluationPDFParser.java
+++ src/main/java/awkwardrobots/util/EvaluationPDFParser.java
@@ -11,6 +11,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EvaluationPDFParser implements CommentParser {
 
@@ -22,19 +24,73 @@ public class EvaluationPDFParser implements CommentParser {
             parser.parse();
             PDDocument pdf = new PDDocument(parser.getDocument());
             PDFTextStripper pdfStripper = new PDFTextStripper();
+
             String text = pdfStripper.getText(pdf);
+
             List<String> lines = Arrays.asList(text.split(System.lineSeparator()));
             Iterator<String> iterator = lines.iterator();
 
             comments = new ArrayList<>();
-            for (String line : lines) {
-                comments.add(new Comment(Sentiment.UNCLEAR, line));
+            Pattern relevant = Pattern.compile("" +
+                    "(Positive Resonanz)|" +
+                    "(Wiederholungen)|" +
+                    "(Verbesserungsvorschläge)");
+            Matcher finder = relevant.matcher("");
+
+
+            while (iterator.hasNext()) {
+                String line = iterator.next();
+                finder.reset(line);
+                if (finder.matches()) {
+                    Sentiment sentiment = getSentiment(line);
+                    comments.addAll(getComments(iterator, sentiment));
+                }
             }
             inputStream.close();
             return comments;
         } catch (IOException e) {
             System.err.println("Could not parse document.");
             e.printStackTrace();
+        }
+
+        return comments;
+    }
+
+    private Sentiment getSentiment(String line) {
+        if (line.equals("Wiederholungen"))
+            return Sentiment.UNCLEAR;
+
+        if (line.equals("Positive Resonanz"))
+            return Sentiment.POSITIVE;
+
+        if (line.equals("Verbesserungsvorschläge"))
+            return Sentiment.NEGATIVE;
+
+        throw new IllegalArgumentException("Line must be the start of a feedback question. Was: " + line);
+    }
+
+    private List<Comment> getComments(Iterator<String> iterator, Sentiment sentiment) {
+        List<Comment> comments = new ArrayList<>();
+        String line = "";
+
+        // skip irrelevant lines;
+        while (!line.equals("Anz"))
+            line = iterator.next();
+
+        while (iterator.hasNext()) {
+            line = iterator.next();
+
+            // break if end of list is reached
+            if (line.startsWith("Summe"))
+                break;
+
+            // separate comment from number of mentions
+            int lastSpace = line.lastIndexOf(' ');
+            String comment = line.substring(0, lastSpace);
+            int mentions = Integer.parseInt(line.substring(lastSpace + 1));
+
+            for (int i = 0; i < mentions; i++)
+                comments.add(new Comment(sentiment, comment));
         }
 
         return comments;
