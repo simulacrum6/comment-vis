a95a21fb75260a1e6ce3eb33d78dcc575a0dd8d1
Matthias <Matthias.Brzezinski@gmx.de>
2019-05-27 00:45:30 +0200

    Chart.js Integration

--- frontend/visualisation-frontend/src/app/pages/visualisation/bar/bar.component.ts
+++ frontend/visualisation-frontend/src/app/pages/visualisation/bar/bar.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {Aspect} from './model';
 import mockData from '../mock.json';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-bar',
@@ -8,8 +9,9 @@ import mockData from '../mock.json';
   styleUrls: ['./bar.component.scss']
 })
 export class BarComponent implements OnInit {
-
   aspects: Aspect[];
+  @ViewChild('barChart') private chartRef;
+  chart: any;
 
   constructor() {
       this.generateAspectsFromMock();
@@ -20,6 +22,28 @@ export class BarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.chart = new Chart(this.chartRef.nativeElement, {
+      type: 'horizontalBar',
+      data: {
+        datasets: [
+          {
+            label: 'Teacher',
+            backgroundColor: 'rgba(210, 214, 222, 1)',
+            data: [10, 20, 30, 40],
+          },
+        ],
+        labels: ['Cool', 'Lazy', 'Nice', 'Boring']
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }
