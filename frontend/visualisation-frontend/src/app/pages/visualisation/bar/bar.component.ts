1bfc9b3a3d7cbddc5bded14bfbb78f737abad585
Matthias <Matthias.Brzezinski@gmx.de>
2019-06-08 20:53:33 +0200

    exported Modeltransformation

--- frontend/visualisation-frontend/src/app/pages/visualisation/bar/bar.component.ts
+++ frontend/visualisation-frontend/src/app/pages/visualisation/bar/bar.component.ts
@@ -1,10 +1,10 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
-import {Chart, ChartDataSets, ChartOptions, ChartType} from 'chart.js';
-import Color from 'color';
-import { ModelService} from '../../../services/model.service';
-import { Aspect } from './model';
+import {Component, OnInit} from '@angular/core';
+import {ChartOptions, ChartType} from 'chart.js';
+import {ModelService} from '../../../services/model.service';
+import {Aspect} from './model';
 import mockData from '../mock.json';
-import {ChartsModule, Label} from 'ng2-charts';
+import {Label} from 'ng2-charts';
+import {ModelTransformation} from './modeltransformation';
 
 
 /*import {Aspect, Extraction, Facet, FacetGroup} from '../../../models/canonical';
@@ -16,9 +16,9 @@ import {Sentiment, SentimentCount} from '../../../models/sentiment';*/
   styleUrls: ['./bar.component.scss']
 })
 export class BarComponent implements OnInit {
-  aspects: Aspect[];
+  private aspects: Aspect[];
+  private modelTransformation: ModelTransformation;
 
-  /** Chart **/
   public chartData: any = [];
   public chartType: ChartType = 'horizontalBar';
   public chartLabels: Label[] = [];
@@ -44,24 +44,20 @@ export class BarComponent implements OnInit {
   };
 
   sortOptions: any = [
-      {value: 'noone', viewValue: 'No Sorting'},
-      {value: 'positive', viewValue: 'Positive Sentiments'},
-      {value: 'neutral', viewValue: 'Neutral Sentiments'},
-      {value: 'negative', viewValue: 'Negative Sentiments'}
-    ];
+    {value: 'noone', viewValue: 'No Sorting'},
+    {value: 'positive', viewValue: 'Positive Sentiments'},
+    {value: 'neutral', viewValue: 'Neutral Sentiments'},
+    {value: 'negative', viewValue: 'Negative Sentiments'}
+  ];
 
   sortOrderOptions: any = [
     {value: 'ascending', viewValue: 'ascending'},
     {value: 'descending', viewValue: 'descending'},
   ];
 
-  chartColors = {
-    red: 'rgb(255, 0, 0)',
-    yellow: 'rgb(255, 205, 86)',
-    green: 'rgb(0, 163, 51)',
-  };
 
   constructor(private modelService: ModelService) {
+    this.modelTransformation = new ModelTransformation(this);
     this.generateAspectsFromMock();
   }
 
@@ -70,47 +66,7 @@ export class BarComponent implements OnInit {
   }
 
   ngOnInit() {
-    const positiveSentiments = [];
-    const neutralSentiments = [];
-    const negativeSentiments = [];
-    const unknownSentiments = [];
-
-    this.aspects.forEach((aspect) => {
-      aspect.bars.forEach((bar) => {
-        this.chartLabels.push(bar.attributeDescription);
-        positiveSentiments.push(bar.positiveSentimentCount);
-        neutralSentiments.push(bar.neutralSentimentCount);
-        negativeSentiments.push(bar.negativeSentimentCount);
-      });
-    });
-
-    let borderColor: string;
-    let label: string;
-    let data = [];
-    for (let i = 0; i < 3; i++) {
-      if (i === 0) {
-        borderColor = this.chartColors.green;
-        label = 'Positive';
-        data = positiveSentiments;
-      } else if (i === 1) {
-        borderColor = this.chartColors.yellow;
-        label = 'Neutral';
-        data = neutralSentiments;
-      } else {
-        borderColor = this.chartColors.red;
-        label = 'Negative';
-        data = negativeSentiments;
-      }
-      const dataSet: any = {};
-      dataSet.borderColor = borderColor;
-      dataSet.borderWidth = 3;
-      dataSet.backgroundColor = Color(borderColor).alpha(0.3).string();
-      dataSet.hoverBorderColor = Color(borderColor).string();
-      dataSet.hoverBackgroundColor = Color(borderColor).alpha(0.6).string();
-      dataSet.label = label;
-      dataSet.data = data;
-      this.chartData.push(dataSet);
-    }
+    this.modelTransformation.buildChartData(this.aspects);
   }
 
 }
---
+++ frontend/visualisation-frontend/src/app/pages/visualisation/bar/modeltransformation.ts
@@ -0,0 +1,64 @@
+import {Aspect} from './model';
+import Color from 'color';
+import {BarComponent} from './bar.component';
+
+export class ModelTransformation {
+
+  private parentComponent: BarComponent;
+
+  private chartColors = {
+    red: 'rgb(255, 0, 0)',
+    gray: 'rgb(169, 169, 169)',
+    green: 'rgb(0, 163, 51)',
+  };
+
+  constructor(parentComponent: BarComponent) {
+    this.parentComponent = parentComponent;
+  }
+
+
+  buildChartData(aspects: Aspect[]): void {
+    const positiveSentiments = [];
+    const neutralSentiments = [];
+    const negativeSentiments = [];
+    const unknownSentiments = [];
+
+    aspects.forEach((aspect) => {
+      aspect.bars.forEach((bar) => {
+        this.parentComponent.chartLabels.push(bar.attributeDescription);
+        positiveSentiments.push(bar.positiveSentimentCount);
+        neutralSentiments.push(bar.neutralSentimentCount);
+        negativeSentiments.push(bar.negativeSentimentCount);
+      });
+    });
+
+    let borderColor: string;
+    let label: string;
+    let data = [];
+    for (let i = 0; i < 3; i++) {
+      if (i === 0) {
+        borderColor = this.chartColors.green;
+        label = 'Positive';
+        data = positiveSentiments;
+      } else if (i === 1) {
+        borderColor = this.chartColors.gray;
+        label = 'Neutral';
+        data = neutralSentiments;
+      } else {
+        borderColor = this.chartColors.red;
+        label = 'Negative';
+        data = negativeSentiments;
+      }
+      const dataSet: any = {};
+      dataSet.borderColor = borderColor;
+      dataSet.borderWidth = 3;
+      dataSet.backgroundColor = Color(borderColor).alpha(0.3).string();
+      dataSet.hoverBorderColor = Color(borderColor).string();
+      dataSet.hoverBackgroundColor = Color(borderColor).alpha(0.6).string();
+      dataSet.label = label;
+      dataSet.data = data;
+      this.parentComponent.chartData.push(dataSet);
+    }
+  }
+
+}
