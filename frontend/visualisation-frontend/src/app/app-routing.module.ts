ed614c21199a06e49c33250bf8b043dfb013e00a
Marius Hamacher <marius.hamacher@outlook.de>
2019-07-05 10:34:10 +0200

    add routing for visualisations.

--- frontend/visualisation-frontend/src/app/app-routing.module.ts
+++ frontend/visualisation-frontend/src/app/app-routing.module.ts
@@ -3,11 +3,21 @@ import { Routes, RouterModule } from '@angular/router';
 import { UploadComponent } from './pages/upload/upload.component';
 import { VisualisationComponent } from './pages/visualisation/visualisation.component';
 import { DatasetOverviewComponent } from './pages/dataset-overview/dataset-overview.component';
+import { PieGridComponent } from './pages/visualisation/pie-grid/pie-grid.component';
+import { TreeMapComponent } from './pages/visualisation/tree-map/tree-map.component';
+import { BarComponent } from './pages/visualisation/bar/bar.component';
 
 const routes: Routes = [
   { path: '', component: UploadComponent },
   { path: 'overview', component: DatasetOverviewComponent },
-  { path: 'vis', component: VisualisationComponent },
+  { path: 'vis', component: VisualisationComponent,
+    children: [
+      { path: '', redirectTo: 'pie', pathMatch: 'full' },
+      { path: 'pie', component: PieGridComponent },
+      { path: 'tree', component: TreeMapComponent },
+      { path: 'bar', component: BarComponent },
+    ]
+  },
 ];
 
 @NgModule({
--- frontend/visualisation-frontend/src/app/pages/visualisation/visualisation.component.ts
+++ frontend/visualisation-frontend/src/app/pages/visualisation/visualisation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ModelService} from '../../services/model.service';
+import { ModelService } from '../../services/model.service';
 import { MatSnackBar } from '@angular/material';
 import { Router } from '@angular/router';
 
@@ -10,13 +10,21 @@ import { Router } from '@angular/router';
 })
 export class VisualisationComponent implements OnInit {
 
+  public readonly visualisationLinks = [
+    { path: 'pie', label: 'Pie' },
+    { path: 'tree', label: 'Tree' },
+    { path: 'bar', label: 'Bar' }
+  ];
+
   constructor(private modelService: ModelService, private snackBar: MatSnackBar, private router: Router) {
-      if (!this.modelService.model) {
-        this.snackBar.open('No data was available. You were redirected to the upload page.', 'Okay');
-        this.router.navigate(['/']);
-      }
+    if (!this.modelService.model) {
+      this.snackBar.open('No data was available. You were redirected to the upload page.', 'Okay', { duration: 3500 });
+      this.router.navigate(['/']);
     }
+  }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
+  public navigate() {
+  }
 }
