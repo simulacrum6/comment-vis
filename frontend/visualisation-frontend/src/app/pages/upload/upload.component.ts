c54c6e33f60b726fb0c32e2c38dcae9b6f803bc2
Matthias <Matthias.Brzezinski@gmx.de>
2019-09-06 15:06:33 +0200

    Upload

--- frontend/visualisation-frontend/src/app/pages/upload/upload.component.ts
+++ frontend/visualisation-frontend/src/app/pages/upload/upload.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSelectChange } from '@angular/material';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Model } from 'src/app/models/canonical';
+import {Model} from 'src/app/models/canonical';
 import { DemoModel, DemoModels } from 'src/app/models/demo';
 import { StateService } from 'src/app/services/state.service';
 
@@ -19,6 +19,7 @@ export class UploadComponent implements OnInit {
   ];
 
   private model: Model;
+  private uploadedInvalidDataset: boolean;
 
   set dataset(dataset: any) {
     if (dataset instanceof MatSelectChange) {
@@ -26,6 +27,7 @@ export class UploadComponent implements OnInit {
     } else {
       this.model = Model.fromJson(dataset);
     }
+    this.uploadedInvalidDataset = !this.model;
   }
 
   constructor(private router: Router, private ar: ActivatedRoute, private stateService: StateService) {
@@ -38,5 +40,24 @@ export class UploadComponent implements OnInit {
     this.stateService.clear();
     this.stateService.model.state = this.model;
     this.router.navigate(['stats']);
+  }
+
+  handleFileInput(files: FileList) {
+    if (files.length < 1) {
+      return;
+    }
+
+    const file = files.item(0);
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = event => {
+      console.log('uploaded file: ' + fileReader.result);
+      try {
+        this.dataset = JSON.parse(fileReader.result);
+      } catch (exception) {
+        this.uploadedInvalidDataset = true;
+      }
+    };
+    fileReader.readAsText(file, 'utf-8');
   }
 }
