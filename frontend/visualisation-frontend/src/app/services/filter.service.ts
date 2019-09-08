4131ad345870f41a83d5165132971f78dea2ef32
Marius Hamacher <marius.hamacher@outlook.de>
2019-09-08 23:20:19 +0200

    enable keep filters for case selection.

--- frontend/visualisation-frontend/src/app/services/filter.service.ts
+++ frontend/visualisation-frontend/src/app/services/filter.service.ts
@@ -4,7 +4,10 @@ import { StateService } from './state.service';
 
 type Filter = (group: ExtractionGroup) => boolean;
 
+type FilterType = 'shun' | 'keep';
+
 class FilterOption {
+  public type: FilterType = 'shun';
   constructor(public readonly name: string, public readonly value: any, public readonly filterFunction: Filter) {}
 }
 
@@ -41,15 +44,24 @@ class FilterGenerator {
   }
 }
 
-
 @Injectable({
   providedIn: 'root'
 })
 export class FilterService {
+
+  /**
+   * Keep Filters are applied before Shun Filters.
+   * If any of their filter function matches the tested ExtractionGroup,
+   * it is kept in the filtered results, regardless of Shun Filters.
+   */
+  private keepFilters: FilterOption[] = [];
+  /**
+   * Shun Filters are applied after Keep Filters.
+   * They work as you would expect Filters to work.
+   */
+  private shunFilters: FilterOption[] = [];
   private filterGenerator: FilterGenerator = new FilterGenerator();
-  private activeFilters: FilterOption[] = [];
   private _data: ExtractionGroup[] = [];
-  private matchType: 'all' | 'any' = 'all';
 
   public get filteredData(): ExtractionGroup[] {
     return this.applyFilters();
@@ -57,66 +69,61 @@ export class FilterService {
   public set data(groups: ExtractionGroup[]) {
     this._data = groups;
   }
-  private get filterFunctions(): Filter[] {
-    return this.activeFilters.map(option => option.filterFunction);
+  private get filterFunction(): Filter {
+    return (group: ExtractionGroup) => {
+      const keeps = this.keepFilters.map(opt => opt.filterFunction);
+      if (keeps.some(f => f(group))) {
+        return true;
+      }
+      const shuns = this.shunFilters.map(opt => opt.filterFunction);
+      return shuns.every(f => f(group));
+    };
   }
 
   constructor(private stateService: StateService) { }
 
-  public add(filter: string, value: any) {
+  public add(filter: string, value: any, type: FilterType = 'shun') {
     // do nothing, if filter is already in activeFilter list
-    if (this.findIndex(filter, value) !== -1) {
+    if (this.findIndex(filter, value, type) !== -1) {
       return;
     }
     const option = this.filterGenerator.generate(filter, value);
-    this.activeFilters.push(option);
+    this.getFilterOptions(type).push(option);
   }
-  public remove(filter: string, value: any) {
+  public remove(filter: string, value: any, type: FilterType = 'shun') {
     const index = this.findIndex(filter, value);
     if (index === -1) {
       console.log(`could not remove filter [${name},${value}], since it is not an active filter`);
     } else {
-      this.activeFilters.splice(index, 1);
+      this.getFilterOptions(type).splice(index, 1);
     }
   }
-  public change(filter: string, oldValue: any, newValue: any) {
-    const index = this.findIndex(filter, oldValue);
+  public change(filter: string, oldValue: any, newValue: any, type: FilterType = 'shun') {
+    const index = this.findIndex(filter, oldValue, type);
     if (index === -1) {
       console.log(`could not change filter [${filter},${oldValue}], since it is not an active filter`);
     } else {
-      this.activeFilters[index] = this.filterGenerator.generate(filter, newValue);
+      this.getFilterOptions(type)[index] = this.filterGenerator.generate(filter, newValue);
     }
   }
+
   /**
-   * Finds the index of the given combination of filter name and value in activeFilters.
-   * Returns -1 if combination is not part of activeFilters.
+   * Finds the index of the given combination of filter name, value and type in the active filters.
+   * Returns -1 if combination is not part of the active filters.
    */
-  private findIndex(filter: string, value: any): number {
-    return this.activeFilters.findIndex(option => {
-      console.log(option.name)
-      console.log(option.value)
-      return (option.name === filter) && (option.value === value)
-    });
+  private findIndex(filter: string, value: any, type: FilterType = 'shun'): number {
+    const finder = (option: FilterOption) => (option.name === filter) && (option.value === value);
+    const options = this.getFilterOptions(type);
+    return options.findIndex(finder);
   }
   /**
    * Applies all active filter functions to the given group.
    */
   private applyFilters(): ExtractionGroup[] {
-    const filterFunction: Filter = this.matchType === 'all' ? this.matchAll.bind(this) : this.matchAny.bind(this);
-    return this._data.filter(filterFunction);
+    const filter = this.filterFunction.bind(this);
+    return this._data.filter(filter);
   }
-  /**
-   * Applies all active filter functions to the given group.
-   * Returns true, if *all* filters apply;
-   */
-  private matchAll(group: ExtractionGroup): boolean {
-    return this.filterFunctions.every(f => f(group));
-  }
-  /**
-   * Applies all active filter functions to the given group.
-   * Returns true, if *some* filter applies;
-   */
-  private matchAny(group: ExtractionGroup): boolean {
-    return this.filterFunctions.some(f => f(group));
+  private getFilterOptions(type: FilterType): FilterOption[] {
+    return type === 'shun' ? this.shunFilters : this.keepFilters;
   }
 }
