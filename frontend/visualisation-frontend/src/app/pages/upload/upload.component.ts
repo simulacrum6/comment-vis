043cd99e2fd2d7f560a093af367d1b19524964c8
Matthias <Matthias.Brzezinski@gmx.de>
2019-09-09 21:29:47 +0200

    Upload: fix

--- frontend/visualisation-frontend/src/app/pages/upload/upload.component.ts
+++ frontend/visualisation-frontend/src/app/pages/upload/upload.component.ts
@@ -1,7 +1,6 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { MatSelectChange } from '@angular/material';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import {Model} from 'src/app/models/canonical';
+import { Model } from 'src/app/models/canonical';
 import { DemoModel, DemoModels } from 'src/app/models/demo';
 import { StateService } from 'src/app/services/state.service';
 
@@ -15,24 +14,17 @@ export class UploadComponent implements OnInit {
   @ViewChild('upload') upload: ElementRef;
 
   private readonly demoDatasets = [
-    { viewValue: 'Foursquare Reviews', value: DemoModel.Foursquare },
-    { viewValue: 'Amazon, Yelp, IMDB Reviews', value: DemoModel.Reviews },
-    { viewValue: 'Lecture Evaluations', value: DemoModel.Evaluations }
+    { viewValue: '--', file: '', value: ''},
+    { viewValue: 'Foursquare Reviews', value: 'foursquare' },
+    { viewValue: 'Amazon, Yelp, IMDB Reviews', value: 'reviews' },
+    { viewValue: 'Lecture Evaluations', value: 'evaluations' }
   ];
 
-  private model: Model;
   private uploadedInvalidDataset: boolean;
   private noDatasetSelected: boolean;
+  private demoValue = this.demoDatasets[0].value;
+  private uploadJson: any = '';
 
-  set dataset(dataset: any) {
-    if (dataset instanceof MatSelectChange) {
-      this.upload.nativeElement.value = '';
-      this.model = DemoModels.getModel(dataset.value);
-    } else {
-      this.model = Model.fromJson(dataset);
-    }
-    this.noDatasetSelected = false;
-  }
 
   constructor(private router: Router, private ar: ActivatedRoute, private stateService: StateService) {
     this.stateService.loadSafe();
@@ -40,35 +32,68 @@ export class UploadComponent implements OnInit {
 
   ngOnInit() { }
 
-  submit(): void {
-    if (!this.model) {
-      this.noDatasetSelected = true;
-      return;
-    }
-
-    this.stateService.clear();
-    this.stateService.model.state = this.model;
-    this.router.navigate(['stats']);
-  }
-
-  handleFileInput(files: FileList) {
+  handleFileUpload(files: FileList) {
     if (files.length < 1) {
+      this.resetUpload();
       return;
     }
+
+    this.resetDemoValue();
 
     const file = files.item(0);
     const fileReader = new FileReader();
 
     this.uploadedInvalidDataset = false;
-    fileReader.onloadend = event => {
+    fileReader.onloadend = () => {
       console.log('uploaded file: ' + fileReader.result);
       try {
-        this.dataset = JSON.parse(fileReader.result as string);
+        this.uploadJson = JSON.parse(fileReader.result as string);
       } catch (exception) {
+        this.resetUpload();
+        this.uploadJson = '';
         this.uploadedInvalidDataset = true;
-        this.model = null;
       }
     };
     fileReader.readAsText(file, 'utf-8');
   }
+
+  private getDemoFileForString(value: string): DemoModel {
+    switch (value) {
+      case 'foursquare': return DemoModel.Foursquare;
+      case 'reviews': return DemoModel.Reviews;
+      case 'evaluations': return DemoModel.Evaluations;
+      default: return null;
+    }
+  }
+
+  private resetUpload() {
+    this.upload.nativeElement.value = '';
+    this.uploadJson = '';
+    this.uploadedInvalidDataset = false;
+  }
+
+  private resetDemoValue() {
+    this.demoValue = '';
+  }
+
+  submit(): void {
+    if (!this.uploadJson && !this.demoValue) {
+      this.noDatasetSelected = true;
+      return;
+    }
+
+    let model: Model;
+    if (this.uploadJson) {
+      model = Model.fromJson(this.uploadJson);
+    } else if (this.demoValue) {
+      model = DemoModels.getModel(this.getDemoFileForString(this.demoValue));
+    } else {
+      return;
+    }
+
+    this.stateService.clear();
+    this.stateService.model.state = model;
+    this.router.navigate(['stats']);
+  }
+
 }
